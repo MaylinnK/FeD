1c2ceb9c5e99e53a5b93c3302cd95d67651cd3ec
MaylinnK <74138768+MaylinnK@users.noreply.github.com>
2020-11-22 00:24:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -1,1 +1,15 @@
-// JavaScript Document
+/*jslint browser: true, devel: true, eqeq: true, plusplus: true, sloppy: true, vars: true, white: true*/
+
+/*eslint-env browser * /
+
+/*eslint 'no-console': 0*/
+
+/* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
+function myFunction() {
+  var x = document.getElementById("myLinks");
+  if (x.style.display === "block") {
+    x.style.display = "none";
+  } else {
+    x.style.display = "block";
+  }
+}
