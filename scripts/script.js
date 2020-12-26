24e82d05000cc092457f35f9eae16433c6ca5fba
MaylinnK <74138768+MaylinnK@users.noreply.github.com>
2020-12-26 14:14:26 +0100

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -5,16 +5,91 @@
 /*eslint 'no-console': 0*/
 
 /* Toggle between showing and hiding the navigation menu links when the user clicks on the hamburger menu / bar icon */
-var nav = document.getElementById("nav");
 
-function hamburger() {
-  console.log("Im working");
-  if (nav.style.display == "block") {
-    nav.style.display = "none";
-    console.log ("none");
-  } else {
-    nav.style.display = "block";
-    console.log ("block");
+document.querySelectorAll('a[href^="#skills"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.querySelectorAll('a[href^="#works"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+document.querySelectorAll('a[href^="#experiments"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+
+document.querySelectorAll('a[href^="#contact"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
+var linkedinLogo = document.querySelector('footer > section > span > a > img').src;
+console.log(linkedinLogo);
+
+var twitterLogo = document.getElementById('twitter').src;
+console.log(twitterLogo);
+
+var twitterStatus = false;
+var linkedStatus = false;
+
+function twitterOn() {
+  if (twitterStatus == false) {
+    document.getElementById('twitter').src = './images/twittertwee.svg';
+    twitterStatus = true;
   }
 }
-document.getElementById("hamburger").addEventListener("click", hamburger);
+
+document.getElementById("twitter").addEventListener("mouseover", twitterOn);
+
+function twitterOff() {
+  if (twitterStatus == true) {
+    document.getElementById('twitter').src = './images/twitter.svg';
+    twitterStatus = false;
+  }
+}
+
+document.getElementById("twitter").addEventListener("mouseout", twitterOff);
+
+
+function linkedOn() {
+  if (linkedStatus == false) {
+    document.querySelector('footer > section > span > a > img').src = "./images/linkedtwee.svg";
+    linkedStatus = true;
+  }
+}
+
+var linkedinLogo = document.querySelector('footer > section > span > a > img').addEventListener("mouseover", linkedOn);
+
+function linkedOff() {
+  if (linkedStatus == true) {
+    var linkedinLogo = document.querySelector('footer > section > span > a > img').src = './images/li.svg';
+    linkedStatus = false;
+  }
+}
+
+var linkedinLogo = document.querySelector('footer > section > span > a > img').addEventListener("mouseout", linkedOff);
